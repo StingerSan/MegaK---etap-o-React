0f1902ec6e1847cdb740c84017939b9eb39e2948
StingerSan <stinger@int.pl>
2024-02-08 10:43:33 +0100

    MegaK - react

--- megak - react/src/UsersList/UsersList.jsx
+++ megak - react/src/UsersList/UsersList.jsx
@@ -1,3 +1,11 @@
-export const UsersList = props => {
-    console.log(props.users)
-};
+import {UsersListOneItem} from "./UsersListOneItem.jsx";
+
+export const UsersList = props => (
+    <ul>
+        {
+            [...props.users]
+                .sort((a, b) => a.username.localeCompare(b.username))
+                .map(userObj => <UsersListOneItem user={userObj} key={userObj.username}/>)
+        }
+    </ul>
+);
---
+++ megak - react/src/UsersList/UsersListOneItem.jsx
@@ -0,0 +1,6 @@
+export const UsersListOneItem =props => (
+    <li key={props.user.username}>
+        <strong>{props.user.first_name} {props.user.last_name}</strong>
+            {props.user.username}
+    </li>
+)
